8d1ba60e5488fa88505ec48a28891869795a9bd7
Jana Gauß <ja.gauss@t-online.de>
2022-04-28 18:16:19 +0200

    plot function as wrapper

--- R/plot_claraclust.R
+++ R/plot_claraclust.R
@@ -3,35 +3,26 @@
 #' Function to provide graphical visualization of distribution
 #' @param x an object of class claraclust
 #' @param data data.frame used for clustering
-#' @param variable name of variable to plot
 #' @param type type of plot
-#' @param group_by grouping variable for scatterplots
 #' @param confidence_threshold threshold for fuzzy clustering observations to
 #' be plotted
-#' @param metric distance metric for silhouette plot, default is euclidean. Irrelevant if silhouette_subsample is TRUE
-#' @param silhouette_subsample use the subsample from claraclust for silhouette plot instead of all samples
-#' @param scale_sil scale numeric variables for silhouette plot? Default TRUE, irrelevant if silhouette_subsample is TRUE
-#' @param plot_all_fuzzy for scatterplot and fuzzy clustering: if confidence threshold > 0, should the observations below the threshold be plotted transparent? Default is FALSE
-#' @param ... additional arguments e.g. for distance metric
+#' @param ... further arguments for plot functions
 #' @return clustering plot
 #' @import ggplot2 dplyr cluster factoextra ggpubr ggsci ggwordcloud
 #' @importFrom stats as.formula prcomp
 #' @export
-
-plot.claraclust <- function(x, data, variable = NULL, type = NULL,
-                            group_by = NULL, confidence_threshold = 0, 
-                            metric = "euclidean", silhouette_subsample = FALSE, scale_sil = TRUE,
-                            plot_all_fuzzy = FALSE,
+plot.claraclust <- function(x, data,
+                            type = NULL,
+                            confidence_threshold = 0,
                             ...) {
 
   # Input checking:
   checkmate::assert(checkmate::check_data_frame(data),
                     checkmate::check_matrix(data), combine = "or")
-  checkmate::assert_character(x = variable, null.ok = TRUE)
   checkmate::assert_list(x = x)
   checkmate::assert_numeric(x = confidence_threshold, lower = 0, upper = 1)
   checkmate::assert_choice(x = type,
-                           choices = c("grouped", "wordclouds", "silhouette",
+                           choices = c("wordclouds", "silhouette",
                                        "pca", "scatterplot"), null.ok = TRUE)
 
   # Convertion of matrix to data.frame:
@@ -39,13 +30,8 @@ plot.claraclust <- function(x, data, variable = NULL, type = NULL,
     data <- as.data.frame(data)
   }
 
-  if (!is.null(variable) && !(variable %in% names(data))) {
-    stop("Dataset does not contain the given variable.")
-  }
-
   # Data preparation:
-
-  data <- data %>% # dplyr::select(!!variable) %>%
+  data <- data %>%
     mutate(cluster = as.factor(x$clustering))
 
   int_vars <- unlist(lapply(data, is.integer))
@@ -53,142 +39,263 @@ plot.claraclust <- function(x, data, variable = NULL, type = NULL,
 
 
   if (x$type == "fuzzy") {
-    # Filter relevant observation based on the membership score threshhold
+    # Filter relevant observation based on the membership score threshold
     relevant_obs <- x$membership_scores %>%
       mutate(max_memb_score = do.call(pmax, c(., na.rm = TRUE))) %>%
       filter(max_memb_score >= confidence_threshold)
-    
-    
-    if(!is.null(type) && type == "scatterplot"){
-      transparent_obs <- data %>% dplyr::filter(!(row.names(data) %in%  rownames(relevant_obs)))
-    }
+
+    # transparent observations for scatterplot and pca
+    transparent_obs <- data %>% dplyr::filter(!(row.names(data) %in%  rownames(relevant_obs)))
 
     data <- data %>% dplyr::filter(row.names(data) %in%  rownames(relevant_obs))
+
+  } else{
+    transparent_obs <- NULL
   }
 
+
+
   # Creation of plot object:
-  if (class(data[, variable]) == "numeric") {
+  if (is.null(type)) {
+    plot <- clara_bar_boxplot(x = x, data = data, ...)
+  }
+  else if (!is.null(type) && type == "wordclouds") {
+    plot <- clara_wordcloud(x = x, data = data, ...)
+  }
+  else if (!is.null(type) && type == "silhouette") {
+    plot <- clara_silhouette(x = x, data = data, ...)
+  }
+  else if (!is.null(type) && type == "pca") {
+    plot <- clara_pca(x = x, data = data, ...)
+  }
+  else if (!is.null(type) && type == "scatterplot") {
+    plot <- clara_scatterplot(x = x, data = data, transparent_obs = transparent_obs, ...)
+  }
+
+
+  # Return plot:
+  return(plot)
+}
+
+
+
+#' Plot function barplot or boxplot
+#'
+#' Function to plot a barplot or a boxplot
+#' @param x an object of class claraclust
+#' @param data prepared data.frame (contains cluster variable, observations are already filtered by threshold (fuzzy))
+#' @param variable name of variable to plot
+#' @param group_by optional grouping variable
+#' @return barplot or boxplot
+#' @import ggplot2 dplyr cluster factoextra ggpubr ggsci ggwordcloud
+#' @export
+clara_bar_boxplot <- function(x, data, variable, group_by = NULL){
+
+  checkmate::assert_character(x = variable)
+  checkmate::assert_character(x = group_by, null.ok = TRUE)
+  if (!(variable %in% names(data))) {
+    stop("Dataset does not contain the given variable.")
+  }
+
+
+  if (class(data[, variable]) == "numeric"){ # boxplot
     plot <- ggplot2::ggplot(data = data,
-                   mapping = aes(x = cluster, y = !!ensym(variable), fill = cluster)) +
+                            mapping = aes(x = cluster, y = !!ensym(variable), fill = cluster)) +
       geom_boxplot() + theme_minimal() +
       scale_fill_npg()
-  }
-  else if (class(data[, variable]) == "factor" & (is.null(type) | !is.null(group_by))) {
+  } else{ # barplot
     plot <- ggplot2::ggplot(data = data,
-                   mapping = aes(x = cluster, fill = !!ensym(variable))) +
+                          mapping = aes(x = cluster, fill = !!ensym(variable))) +
       geom_bar(position = "fill") + theme_minimal() +
       scale_fill_brewer(palette = "Accent")
   }
 
-  if (!is.null(type) && type == "grouped") {
-    if (is.null(group_by))  stop("Please specify the grouping variable.")
-
+  if(!is.null(group_by)){
     if (!(group_by %in% names(data))) {
       stop("Dataset does not contain the given grouping variable.")
     }
 
     plot <- plot + facet_wrap(as.formula(paste("~", group_by)))
-
-  } else if (!is.null(type) && type == "wordclouds") {
-    set.seed(42)
-    plot <- data %>%
-      dplyr::group_by(cluster) %>%
-      dplyr::count(!!ensym(variable)) %>%
-      mutate(angle = 90 * sample(c(0, 1), n(), replace = TRUE, prob = c(60, 40))) %>%
-      ggplot(
-        aes(
-          label = !!ensym(variable),
-          color = cluster,
-          angle = angle
-        )
-      ) +
-      geom_text_wordcloud_area(area_corr_power = 1) +
-      ggplot2::scale_size_area(max_size = 4) +
-      theme_minimal() +
-      facet_wrap(~ cluster) +
-      scale_color_npg()
-
-  } else if (!is.null(type) && type == "silhouette") {
-
-    if(scale_sil == TRUE){
-      ind <- unlist(lapply(data, is.numeric), use.names = TRUE)
-      for (i in ind) {
-        data[, ind] <- scale(data[, ind])
-      }
-    }
-    
-    
-    if(silhouette_subsample == FALSE){
-      sil <- silhouette(as.numeric(data$cluster), dist(select(data, -cluster), method = metric, ...))
-    } else { # use only subsamples from claraclust in order to not calculate the distance matrix between all samples
-      data_sub <- data[x$subsample_ids, ]
-      sil <- silhouette(as.numeric(data_sub$cluster), x$dist_matrix)
-    }
-
-    plot <- fviz_silhouette(sil) + theme_minimal() +
-      scale_fill_npg() + scale_color_npg() +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
-  } else if (!is.null(type) && type == "pca") {
-
-    num_vars <- unlist(lapply(data, is.numeric))
-
-    # Dimension reduction using PCA
-    pca_result <- stats::prcomp(data[, num_vars],  scale = TRUE)
-    individuals_coord <- as.data.frame(get_pca_ind(pca_result)$coord)
-
-    # Add clusters
-    individuals_coord$cluster <- data$cluster
-
-    if (!is.null(group_by)) {
-      individuals_coord[, group_by] <- data[, group_by]
-    }
-
-    # Compute the eigenvalues
-    eigenvalue <- round(get_eigenvalue(pca_result), 1)
-    variance_perc <- eigenvalue$variance.percent
-
-
-    if (!is.null(group_by)) {
-      plot <- ggscatter(
-        individuals_coord, x = "Dim.1", y = "Dim.2",
-        color = "cluster", palette = "npg", ellipse = TRUE, ellipse.type = "convex",
-        shape = group_by, size = 1.5,  legend = "right", ggtheme = theme_bw(),
-        xlab = paste0("Dim 1 (", variance_perc[1], "% )" ),
-        ylab = paste0("Dim 2 (", variance_perc[2], "% )" )
-      ) + stat_mean(aes(color = cluster), size = 4) + theme_minimal()
-    } else {
-      plot <- ggscatter(
-        individuals_coord, x = "Dim.1", y = "Dim.2",
-        color = "cluster", palette = "npg", ellipse = TRUE, ellipse.type = "convex",
-        size = 1.5,  legend = "right", ggtheme = theme_bw(),
-        xlab = paste0("Dim 1 (", variance_perc[1], "% )" ),
-        ylab = paste0("Dim 2 (", variance_perc[2], "% )" )
-      ) + stat_mean(aes(color = cluster), size = 4) + theme_minimal()
-    }
-
-  } else if (!is.null(type) && type == "scatterplot") {
-
-    if (((!(!is.null(variable) & !is.null(group_by)) ) | !(class(data[, variable]) == "numeric" & class(data[, group_by]) == "numeric"))) {
-      stop("Please specify the variables correctly. Both variable and group_by should contain the names of metric variables.")
-    }
-
-    plot <- data %>%
-      ggplot(aes(x = !!ensym(variable), y = !!ensym(group_by), color = cluster) )+
-      geom_point() +
-      geom_smooth(method = "lm") +
-      theme_minimal()  +
-      scale_color_npg()
-    
-    if(x$type == "fuzzy" && plot_all_fuzzy == TRUE){
-      plot <- plot +
-        geom_point(data = transparent_obs, aes(x = !!ensym(variable), y = !!ensym(group_by)), alpha = 0.4)
-    }
-
   }
-  # Return of the plot:
+
   return(plot)
+
+
 }
 
+
+#' Plot function wordcloud
+#'
+#' Function to plot a wordcloud
+#' @param x an object of class claraclust
+#' @param data prepared data.frame (contains cluster variable, observations are already filtered by threshold (fuzzy))
+#' @param variable name of variable to plot
+#' @param seed optional seed
+#' @return wordcloud plot
+#' @import ggplot2 dplyr cluster factoextra ggpubr ggsci ggwordcloud
+#' @export
+clara_wordcloud <- function(x, data, variable, seed = 42){
+
+  checkmate::assert_character(x = variable)
+  if (!(variable %in% names(data))) {
+    stop("Dataset does not contain the given variable.")
+  }
+
+  data$var <- data[, variable] # dplyr::count() doesn't work with !!ensym(variable)
+
+  set.seed(seed)
+  plot <- data %>%
+    dplyr::group_by(cluster) %>%
+    dplyr::count(var) %>%
+    mutate(angle = 90 * sample(c(0, 1), n(), replace = TRUE, prob = c(60, 40))) %>%
+    ggplot(
+      aes(
+        label = var,
+        color = cluster,
+        angle = angle
+      )
+    ) +
+    geom_text_wordcloud_area(area_corr_power = 1) +
+    ggplot2::scale_size_area(max_size = 4) +
+    theme_minimal() +
+    facet_wrap(~ cluster) +
+    scale_color_npg()
+
+  return(plot)
+
+
+}
+
+
+#' Plot function PCA
+#'
+#' Function to plot PCA results
+#' @param x an object of class claraclust
+#' @param data prepared data.frame (contains cluster variable, observations are already filtered by threshold (fuzzy))
+#' @param group_by optional grouping variable
+#' @return PCA plot
+#' @import ggplot2 dplyr cluster factoextra ggpubr ggsci ggwordcloud
+#' @importFrom stats as.formula prcomp
+#' @export
+clara_pca <- function(x, data, group_by = NULL){
+
+  checkmate::assert_character(x = group_by, null.ok = TRUE)
+
+  num_vars <- unlist(lapply(data, is.numeric))
+
+  # Dimension reduction using PCA
+  pca_result <- stats::prcomp(data[, num_vars],  scale = TRUE)
+  individuals_coord <- as.data.frame(get_pca_ind(pca_result)$coord)
+
+  # Add clusters
+  individuals_coord$cluster <- data$cluster
+
+  if (!is.null(group_by)) {
+    individuals_coord[, group_by] <- data[, group_by]
+  }
+
+  # Compute the eigenvalues
+  eigenvalue <- round(get_eigenvalue(pca_result), 1)
+  variance_perc <- eigenvalue$variance.percent
+
+
+  if (!is.null(group_by)) {
+    plot <- ggscatter(
+      individuals_coord, x = "Dim.1", y = "Dim.2",
+      color = "cluster", palette = "npg", ellipse = TRUE, ellipse.type = "convex",
+      shape = group_by, size = 1.5,  legend = "right", ggtheme = theme_bw(),
+      xlab = paste0("Dim 1 (", variance_perc[1], "% )" ),
+      ylab = paste0("Dim 2 (", variance_perc[2], "% )" )
+    ) + stat_mean(aes(color = cluster), size = 4) + theme_minimal()
+  } else {
+    plot <- ggscatter(
+      individuals_coord, x = "Dim.1", y = "Dim.2",
+      color = "cluster", palette = "npg", ellipse = TRUE, ellipse.type = "convex",
+      size = 1.5,  legend = "right", ggtheme = theme_bw(),
+      xlab = paste0("Dim 1 (", variance_perc[1], "% )" ),
+      ylab = paste0("Dim 2 (", variance_perc[2], "% )" )
+    ) + stat_mean(aes(color = cluster), size = 4) + theme_minimal()
+  }
+
+  return(plot)
+
+
+}
+
+
+#' Plot function scatterplot
+#'
+#' Function to plot a scatterplot
+#' @param x an object of class claraclust
+#' @param data prepared data.frame (contains cluster variable, observations are already filtered by threshold (fuzzy))
+#' @param x_var name of x variable
+#' @param y_var name of y variable
+#' @return scatterplot
+#' @import ggplot2 dplyr cluster factoextra ggpubr ggsci ggwordcloud
+#' @export
+clara_scatterplot <- function(x, data, x_var, y_var, transparent_obs = NULL, plot_all_fuzzy = FALSE){
+
+  if (((!(!is.null(x_var) & !is.null(y_var)) ) | !(class(data[, x_var]) == "numeric" & class(data[, y_var]) == "numeric"))) {
+    stop("Please specify the variables correctly. Both variable and group_by should contain the names of metric variables.")
+  }
+
+  plot <- data %>%
+    ggplot(aes(x = !!ensym(x_var), y = !!ensym(y_var), color = cluster) )+
+    geom_point() +
+    geom_smooth(method = "lm") +
+    theme_minimal()  +
+    scale_color_npg()
+
+  if(x$type == "fuzzy" && plot_all_fuzzy == TRUE){
+    plot <- plot +
+      geom_point(data = transparent_obs, aes(x = !!ensym(x_var), y = !!ensym(y_var)), alpha = 0.4)
+  }
+
+  return(plot)
+
+
+}
+
+
+#' Plot function silhouette
+#'
+#' Function to plot a scatterplot
+#' @param x an object of class claraclust
+#' @param data prepared data.frame (contains cluster variable, observations are already filtered by threshold (fuzzy))
+#' @param metric distance metric for silhouette plot, default is euclidean. Irrelevant if silhouette_subsample is TRUE
+#' @param silhouette_subsample use the subsample from claraclust for silhouette plot instead of all samples
+#' @param scale_sil scale numeric variables for silhouette plot? Default TRUE, irrelevant if silhouette_subsample is TRUE
+#' @return silhouette plot
+#' @import ggplot2 dplyr cluster factoextra ggpubr ggsci ggwordcloud
+#' @export
+clara_silhouette <- function(x, data,
+                             metric = "Euclidean",
+                             silhouette_subsample = FALSE,
+                             scale_sil = TRUE){
+
+  if(scale_sil == TRUE){
+    ind <- unlist(lapply(data, is.numeric), use.names = TRUE)
+    for (i in ind) {
+      data[, ind] <- scale(data[, ind])
+    }
+  }
+
+
+  if(silhouette_subsample == FALSE){
+    sil <- silhouette(as.numeric(data$cluster), dist(select(data, -cluster), method = metric))
+  } else { # use only subsamples from claraclust in order to not calculate the distance matrix between all samples
+    data_sub <- data[x$subsample_ids, ]
+    sil <- silhouette(as.numeric(data_sub$cluster), x$dist_matrix)
+  }
+
+  plot <- fviz_silhouette(sil) + theme_minimal() +
+    scale_fill_npg() + scale_color_npg() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+  return(plot)
+
+
+}
 
 
--- tests/testthat/test-plot_claraclust.R
+++ tests/testthat/test-plot_claraclust.R
@@ -1,7 +1,7 @@
 test_that("plot_claraclust", { # plots
-  
+
   data(USArrests)
-  
+
   cc_fixed <- claraclust(data        = USArrests,
                          clusters    = 3,
                          metric      = "euclidean",
@@ -10,8 +10,8 @@ test_that("plot_claraclust", { # plots
                          type        = "fixed",
                          seed        = 3526,
                          verbose     = 0)
-  
-  USArrests_enriched <- USArrests %>% 
+
+  USArrests_enriched <- USArrests %>%
     mutate(State = as.factor(rownames(USArrests)),
            Area  = as.factor(case_when(State %in% c("Washington", "Oregon",
                                                     "California", "Nevada", "Arizona", "Idaho", "Montana",
@@ -28,84 +28,99 @@ test_that("plot_claraclust", { # plots
                                                     "Massachusetts", "Rhode Island", "Vermont", "Pennsylvania",
                                                     "New Jersey", "Connecticut", "Delaware", "Maryland") ~
                                          "Northeast")))
-  
+
   ## error for wrong variable specification
   expect_error(plot(x = cc_fixed, data = USArrests_enriched, variable = "Assalt"))
-  
+
   ## convert data if class is not data.frame
-  p <- plot(x = cc_fixed, data = as.matrix(USArrests_enriched[, 1:2]), variable = "Assault") 
-  # check class of object 
+  p <- plot(x = cc_fixed, data = as.matrix(USArrests_enriched[, 1:2]), variable = "Assault")
+  # check class of object
   expect_s3_class(p, "ggplot")
-  
+
+
   ## Boxplot
-  p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "Assault") 
-  # check class of object 
+  p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "Assault")
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomBoxplot")
-  
+
+  # Grouped Boxplot
+  p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "Assault", group_by = "Area")
+  # check class of object
+  expect_s3_class(p, "ggplot")
+  expect_s3_class(p$layers[[1]]$geom, "GeomBoxplot")
+
+
   ## Barplot
-  p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "Area") 
-  # check class of object 
+  p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "Area")
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomBar")
-  
+
+  ## Grouped Barplot
+  p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "Area", group_by = "State")
+  # check class of object
+  expect_s3_class(p, "ggplot")
+  expect_s3_class(p$layers[[1]]$geom, "GeomBar")
+
+
   ## Wordcloud
   p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "State",
-            type = "wordclouds") 
-  # check class of object 
+            type = "wordclouds")
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomTextWordcloud")
-  
-  ## grouped plot
-  p <- plot(x = cc_fixed, data = USArrests_enriched, variable = "Assault", group_by = "Area", type = "grouped")
-  # check class of object 
-  expect_s3_class(p, "ggplot")
-  expect_s3_class(p$layers[[1]]$geom, "GeomBoxplot")
-  
-  expect_error(plot(x = cc_fixed, data = USArrests_enriched, 
-                    variable = "Assault", type = "grouped"))
-  expect_error(plot(x = cc_fixed, data = USArrests_enriched, 
-                    variable = "Assault", group_by = "area", type = "grouped"))
-  
+
+
   ## Scatterplot
   p <- plot(x = cc_fixed, data = USArrests_enriched, type = "scatterplot",
-            variable = "Murder", group_by = "Assault")
-  # check class of object 
+            x_var = "Murder", y_var = "Assault")
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomPoint")
-  
+
   expect_error(plot(x = cc_fixed, data = USArrests_enriched, type = "scatterplot",
-                    variable = "Murder", group_by = "Area"))
-  
+                    x_var = "Murder", y_var = "Area"))
+
   ## PCA
   p <- plot(x = cc_fixed, data = USArrests_enriched, type = "pca")
-  # check class of object 
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomPoint")
-  
+
   p <- plot(x = cc_fixed, data = USArrests_enriched, type = "pca", group_by = "Area")
-  # check class of object 
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomPoint")
-  
+
   ## Silhouette
-  invisible(capture.output(p <- plot(x = cc_fixed, data = USArrests, type = "silhouette", 
-                                     silhouette_subsample = TRUE))) 
-  # check class of object 
+  invisible(capture.output(p <- plot(x = cc_fixed, data = USArrests, type = "silhouette",
+                                     silhouette_subsample = TRUE)))
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomBar")
-  
-  invisible(capture.output(p <- plot(x = cc_fixed, data = USArrests, type = "silhouette"))) 
-  # check class of object 
+
+  invisible(capture.output(p <- plot(x = cc_fixed, data = USArrests, type = "silhouette")))
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomBar")
-  
+
   # compare silhouette values to manually calculated values
   dat_sil <- p$data
   data <- scale(USArrests)
   dat_sil_manual <- silhouette(as.numeric(cc_fixed$clustering), dist(data))
   expect_identical(round(sort(dat_sil$sil_width), 2), round(sort(dat_sil_manual[, 3]), 2))
-  
+
+
+  # errors:
+  expect_error(plot(x = cc_fixed, data = USArrests_enriched,
+                    variable = "Assault", group_by = "area"))
+  expect_error(plot(x = cc_fixed, data = USArrests_enriched,
+                    variable = "Area", group_by = "area"))
+  expect_error(plot(x = cc_fixed, data = USArrests_enriched, variable = "state",
+                    type = "wordclouds"))
+
+
   ## confidence threshold for fuzzy clustering
   cc_fuzzy <- claraclust(data        = USArrests,
                          clusters    = 3,
@@ -116,20 +131,20 @@ test_that("plot_claraclust", { # plots
                          m           = 2,
                          seed        = 3526,
                          verbose     = 0)
-  
+
   p <- plot(x = cc_fuzzy, data = USArrests_enriched, type = "scatterplot",
-            variable = "Murder", group_by = "Assault", confidence_threshold = 0.5, 
+            x_var = "Murder", y_var = "Assault", confidence_threshold = 0.5,
             plot_all_fuzzy = TRUE)
-  # check class of object 
+  # check class of object
   expect_s3_class(p, "ggplot")
   expect_s3_class(p$layers[[1]]$geom, "GeomPoint")
-  
+
   # check if number of observations for plot is right
-  n_obs_conf <- sum(pmax(cc_fuzzy$membership_scores$Cluster1, 
-                         cc_fuzzy$membership_scores$Cluster2, 
+  n_obs_conf <- sum(pmax(cc_fuzzy$membership_scores$Cluster1,
+                         cc_fuzzy$membership_scores$Cluster2,
                          cc_fuzzy$membership_scores$Cluster3) >= 0.5)
   expect_identical(n_obs_conf, nrow(p$data))
-  
-  
+
+
 })
 
