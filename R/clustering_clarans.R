9690dad117973c703bae0d38e7c769f21343cbcc
ri87nix <maximilian.weigert@stat.uni-muenchen.de>
2022-07-11 17:42:04 +0200

    some improvements clarans clustering

--- R/clustering_clarans.R
+++ R/clustering_clarans.R
@@ -40,19 +40,59 @@ clustering_clarans <- function(data, clusters = 5, metric = "euclidean",
   medoids <- sample(x = 1:nrow(data), size = clusters, replace = FALSE)
 
   # Assign all observations to closest medoid:
+  data <- data %>% mutate(Name = row.names(.)) %>% arrange(Name)
+  data_medoids <- data %>% filter(Name %in% medoids)
 
-  # for each cluster:
-  num_neighbors <- 1
-  # while loop for tested medoids by cluster:
-  while (num_neighbors < max_neighbors) {
+  # Calculate the distances to the cluster medoids:
+  dist_dat <- proxy::dist(x = data[, -ncol(data)],
+                          y = data_medoids[, -ncol(data_medoids)],
+                          method = metric)
 
-    # Compute sum of distances with assigned medoid:
+  # Assign all observations to the closest medoid:
+  # Assignment to the medoid with minimum distance:
+  data$clustering <- apply(dist_dat, 1, which.min)
 
-    # Compute sum of distances with new medoid:
+  # For each clustering iteration:
+  iteration <- 1
+  while (iteration <= num_local) {
 
-    # Raise num_neighbors if old medoid has lower distance:
-    if (1 + 1) {
-      num_neighbors <- num_neighbors + 1
+    # For each cluster:
+    if (cores == 1) { # single core
+      iteration_results <- lapply(X = 1:cluster, FUN = function(i) {
+
+        # Sample medoids:
+        data_cluster <- data %>% filter(clustering == i)
+        sample_meds <- sample(x = 1:nrow(data), size = max_neighbors,
+                              replace = FALSE)
+
+        # Test of other medoids:
+        neighbor <- 0
+        while (neighbor <= max_neighbors) {
+
+          # Computation of membership scores in case of fuzzy clustering:
+          if (type == "fuzzy") {
+            memb_scores_list <- apply(dist_dat, 1, function(x) {
+              data.frame(t(as.numeric(calculate_memb_score(dist_med = x, m = m))))
+            })
+            memb_scores <- dplyr::bind_rows(memb_scores_list)
+            colnames(memb_scores) <- paste0("Cluster", 1:ncol(memb_scores))
+          }
+
+          # Computation of distance for fixed and fuzzy clustering:
+          if (type == "fixed") {
+            # Minimum distance:
+            distances <- apply(dist_dat, 1, min)
+          } else { # type = "fuzzy"
+            # Weighted distance (membership scores as weights):
+            distances <- rowSums(dist_dat * memb_scores)
+          }
+
+          # Raise num_neighbors if old medoid has lower distance:
+          if (1 + 1) {
+            num_neighbors <- num_neighbors + 1
+          }
+        }
+      })
     }
   }
 
