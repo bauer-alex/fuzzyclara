ee855b159accc2381e0ec58e1f6fdfeef360ec95
bauer-alex <bauer-alex@users.noreply.github.com>
2022-08-23 16:53:24 +0000

    Built site for fuzzyclara: 0.1@7e9f7cb

--- reference/figures/hex-sticker/hex-sticker.R
+++ reference/figures/hex-sticker/hex-sticker.R
@@ -21,10 +21,10 @@ dat_points <- dplyr::bind_rows(dat_norm1 %>% sample_n(5),
 
 # main plot ---------------------------------------------------------------
 gg <- ggplot() +
-  geom_density_2d(data = dat_norm1, aes(x = x, y = y), col = "firebrick", h = c(1,1), bins = 4) +
-  geom_density_2d(data = dat_norm2, aes(x = x, y = y), col = "dodgerblue3", h = c(1,1), bins = 4) +
-  geom_density_2d(data = dat_norm3, aes(x = x, y = y), col = "orange", h = c(1,1), bins = 4) +
-  geom_point(data = dat_points, aes(x = x, y = y), col = gray(.4)) +
+  geom_density_2d(data = dat_norm1, aes(x = x, y = y), col = "#995C88", h = c(1,1), bins = 4) +
+  geom_density_2d(data = dat_norm2, aes(x = x, y = y), col = "#ED6445", h = c(1,1), bins = 4) +
+  geom_density_2d(data = dat_norm3, aes(x = x, y = y), col = "#FFB200", h = c(1,1), bins = 4) +
+  geom_point(data = dat_points, aes(x = x, y = y), col = "#277BC0") +
   theme(axis.title       = element_blank(),
         axis.text        = element_blank(),
         axis.ticks       = element_blank(),
@@ -40,9 +40,10 @@ sticker(subplot  = gg,
         package  = "fuzzyclara",
         filename = "fuzzyclara.png",
         p_size   = 20,  # title size
+        p_color  = "#FFB200",
         s_x      = 1,   # plot positioning
         s_y      = .75,
         s_width  = 1.5, # plot size
         s_height = 1.2,
-        h_fill   = "lightgray", # hexagon color
-        h_color  = "darkgreen")
+        h_fill   = "#FFF4CF", # hexagon color
+        h_color  = "#277BC0")
