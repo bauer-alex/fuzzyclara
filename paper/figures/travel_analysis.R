3c6150a5045dfe18c4629dadbd0ecdf454eaf1e1
bauer-alex <alexander.bauer@stat.uni-muenchen.de>
2022-09-01 10:07:38 -0500

    travel analysis

--- paper/figures/travel_analysis.R
+++ paper/figures/travel_analysis.R
@@ -42,7 +42,7 @@ ggsave("travel_description.png", bg = "white", width = 5, height = 4)
 # cluster data ------------------------------------------------------------
 # select number of clusters
 choice <- evaluate_cluster_numbers(data           = dat,
-                                   clusters_range = 2:10,
+                                   clusters_range = 1:10,
                                    metric         = "euclidean",
                                    samples        = 20,
                                    sample_size    = 1000,
@@ -56,11 +56,13 @@ choice$plot
 ggsave("travel_ellbow.png", bg = "white", width = 10, height = 4)
 
 
+# choose the solution with 4 clusters
+n_clusters <- 4
 dat_long <- dat_long %>% 
-  mutate(cluster = paste("Cluster", rep(choice$cluster_results[[5]]$clustering, each = 4)),
+  mutate(cluster = paste("Cluster", rep(choice$cluster_results[[n_clusters]]$clustering, each = 4)),
          cluster = factor(cluster))
 dat_long_medoids <- dat_long %>% 
-  filter(traveler_id %in% choice$cluster_results[[5]]$medoids)
+  filter(traveler_id %in% choice$cluster_results[[n_clusters]]$medoids)
 
 
 
@@ -75,9 +77,25 @@ ggplot(mapping = aes(x = variable, y = value, group = traveler_id, col = cluster
   geom_line(data = dat_long %>% filter(traveler_id %in% random_ids), alpha = 0.05) +
   geom_line(data = dat_long_medoids, size = 1.5) +
   ylab("standardized value") +
+  ylim(c(-2,4)) +
   facet_wrap(~ cluster, nrow = 1) +
   theme_minimal(base_size = 12) +
   theme(axis.title.x    = element_blank(),
         legend.position = "none",
         axis.text.x     = element_text(angle = 45, hjust = 1))
 ggsave("travel_clustered.png", bg = "white", width = 10, height = 4)
+
+
+
+# share of core clusters --------------------------------------------------
+threshold_coreCluster <- 0.6
+
+n_cluster_memberships <- table(choice$cluster_results[[4]]$clustering)
+memb_table                  <- choice$cluster_results[[4]]$membership_scores
+rows_membTable_coreClusters <- which(apply(memb_table, 1, max) > threshold_coreCluster)
+memb_table_coreClusters     <- memb_table[rows_membTable_coreClusters,]
+coreCluster_vector          <- apply(memb_table_coreClusters, 1, which.max)
+n_coreCluster_memberships   <- table(coreCluster_vector)
+
+# core cluster share
+n_coreCluster_memberships / n_cluster_memberships
